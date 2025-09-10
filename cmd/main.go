30611e3ac830666041b8fdd559183867bd98b362
Toni Tienda <tonitienda@gmail.com>
2025-09-10 20:33:53 +0200

    adds the small golang server

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"message": "ok"})
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		color := os.Getenv("COLOR")
+		resp := map[string]string{
+			"ts":    time.Now().Format("2006-01-02T15:04:05.000Z07:00"),
+			"color": color,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saasuke-labs/bluegreen-vps
+
+go 1.24.0
