9c003fddf13b0533b9d45ab2d1925f1de77edf14
Toni Tienda <tonitienda@gmail.com>
2025-09-11 22:17:03 +0200

    pass port and color as args

--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,35 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
+	var port = flag.String("port", "", "Port to listen on")
+	var color = flag.String("color", "", "Color value to return in responses")
+
+	flag.Parse()
+
+	if *port == "" || *color == "" {
+		panic("port and color are required")
+	}
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{"message": "ok"})
 	})
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		color := os.Getenv("COLOR")
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		resp := map[string]string{
 			"ts":    time.Now().Format("2006-01-02T15:04:05.000Z07:00"),
-			"color": color,
+			"color": *color,
 		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(resp)
 	})
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":"+*port, nil)
 }
